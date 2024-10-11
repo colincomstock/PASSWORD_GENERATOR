c70ffb04033e1a88f295ba26ef6d499c16eb9146
colincomstock <95597475+colincomstock@users.noreply.github.com>
2024-10-10 21:45:35 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,115 @@
+const characters = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"~",
+	"`",
+	"!",
+	"@",
+	"#",
+	"$",
+	"%",
+	"^",
+	"&",
+	"*",
+	"(",
+	")",
+	"_",
+	"-",
+	"+",
+	"=",
+	"{",
+	"[",
+	"}",
+	"]",
+	",",
+	"|",
+	":",
+	";",
+	"<",
+	">",
+	".",
+	"?",
+	"/",
+];
+
+let passButton = document.getElementById("pass-btn");
+passButton.addEventListener("click", showPasswords);
+
+let passbox1 = document.getElementById("pass1");
+let passbox2 = document.getElementById("pass2");
+
+function passwordGen() {
+	let passLength = 15;
+	let password = "";
+	for (let i = 0; i <= passLength; i++) {
+		let item = characters[Math.floor(Math.random() * characters.length)];
+		password += item;
+	}
+
+	return password;
+}
+
+function showPasswords() {
+	passbox1.textContent = passwordGen();
+	passbox2.textContent = passwordGen();
+}
